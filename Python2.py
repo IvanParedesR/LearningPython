986f4fa90e40a4d4744f58775cd4fa74c498ffab
Ivan Paredes <ivanresendiz25@gmail.com>
2020-05-06 18:18:25 -0500

    Local change

--- Python2.py
+++ Python2.py
@@ -165,3 +165,60 @@ for line in hand:
     x = re.findall('\S+@\S+', line)
     if len(x) > 0:
         print(x)
+
+# Search for lines that have an at sign between characters
+# The characters must be a letter or number
+import re
+hand = open('mbox.txt')
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('[a-zA-Z0-9]\S+@\S+[a-zA-Z]', line)
+    if len(x) > 0:
+        print(x)
+
+# Search for lines that start with 'X' followed by any non
+# whitespace characters and ':'
+# followed by a space and any number.
+# The number can include a decimal.
+hand = open('mbox.txt')
+for line in hand:
+    line = line.rstrip()
+    if re.search('^X\S*: [0-9.]+', line):
+        print(line)
+        
+
+# Search for lines that start with 'X' followed by any
+# non whitespace characters and ':' followed by a space
+# and any number. The number can include a decimal.
+# Then print the number if it is greater than zero.
+import re
+hand = open('mbox.txt')
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('^X\S*: ([0-9.]+)', line)
+    if len(x) > 0:
+        print(x)
+        
+# Search for lines that start with 'Details: rev='
+# followed by numbers and '.'
+# Then print the number if it is greater than zero
+import re
+hand = open('mbox.txt')
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('^Details:.*rev=([0-9.]+)', line)
+    if len(x) > 0:
+        print(x)
+        
+# Search for lines that start with From and a character
+# followed by a two digit number between 00 and 99 followed by ':'
+# Then print the number if it is greater than zero
+import re
+hand = open('mbox.txt')
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('^From .* ([0-9][0-9]):', line)
+    if len(x) > 0: print(x)
+    
+x = 'We just received $10.00 for cookies.'
+y = re.findall('\$[0-9.]+',x)
